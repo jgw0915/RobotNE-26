849783221b174c9e17dc7612afc5ff046c66ca29
jgw0915 <jgw.0915.tpc@gmail.com>
2026-04-13 00:01:49 +0800

    Upload report and modify rl_play.py

--- HW3 - Deep Reinforcement Learning/HW3-2/rl_play.py
+++ HW3 - Deep Reinforcement Learning/HW3-2/rl_play.py
@@ -247,7 +247,9 @@ class MLPlay:
             print(f"Model saved to {update_path}")
 
     def _predict_action(self, obs):
-        obs_tensor = torch.as_tensor(obs).unsqueeze(0)
+        # obs_tensor = torch.as_tensor(obs).unsqueeze(0)
+        # change to tensor and move to the same device as the model
+        obs_tensor = torch.from_numpy(obs).float().unsqueeze(0).to(self.model.device)
         with torch.no_grad():
             action, value, log_prob = self.model.policy(obs_tensor)
         return action.cpu().numpy().flatten(), log_prob.cpu().numpy().flatten(), value.cpu().numpy().flatten()
